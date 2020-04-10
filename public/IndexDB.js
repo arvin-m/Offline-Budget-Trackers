96d4ef4634fb80a9c75682119f7efb38cb46db41
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-04-10 17:25:23 -0400

    adding manifest and service worker file

--- public/IndexDB.js
+++ public/IndexDB.js
@@ -1,8 +1,8 @@
 // check if the browser support IndexDB
 if (!('indexedDB' in window)) {
     console.log('This browser doesn\'t support IndexedDB');
-    return;
-  };
+    };
+
   let db;
   // create a new db request for a "budget" database.
   const request = indexedDB.open("budget", 1);
