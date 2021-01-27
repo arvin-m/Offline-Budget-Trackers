2eb7a6f37ed7b27bd2cd583ed11a162a38158acf
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2021-01-27 12:29:32 -0500

    fixing the databse name

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-let MONGODB_URI=process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI,{
+
+mongoose.connect(process.env.MONGODB_URI,{
   useNewUrlParser: true,
   useFindAndModify: false
 });
